8c8a09a61ca3f86d1cb5767308791253c6a31ed7
Carolina Acuna <carolina.acuna90@gmail.com>
2023-07-14 17:01:57 +0200

    add item

--- app.js
+++ app.js
@@ -18,4 +18,28 @@ form.addEventListener("submit", addItem);
 //*** Functions *** /
 function addItem(e) {
   e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title">${value}</p>
+    < class="btn-container">
+       <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+    </div>
+    `;
+
+    //add event listeners to both buttons;
+  }
 }
